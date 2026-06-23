b4061b4761e2573b22bb62577fcf1fdfb12dd46d
sidymohamed12 <mohamedsaizonou86@gmail.com>
2026-06-23 10:00:55 +0000

    first commit

---
+++ src/main/java/ministere/sante/senpna/SenpnaApplication.java
@@ -0,0 +1,13 @@
+package ministere.sante.senpna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenpnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenpnaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ministere/sante/senpna/SenpnaApplicationTests.java
@@ -0,0 +1,13 @@
+package ministere.sante.senpna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenpnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
